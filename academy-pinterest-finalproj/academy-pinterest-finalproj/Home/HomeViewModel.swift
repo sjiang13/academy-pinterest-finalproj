5de1c74df8de1dd40786a82a8891db8efe5ecd01
Sophie Jiang <sjiang13@email.unc.edu>
2023-04-18 19:38:34 -0400

    added more images

--- academy-pinterest-finalproj/academy-pinterest-finalproj/Home/HomeViewModel.swift
+++ academy-pinterest-finalproj/academy-pinterest-finalproj/Home/HomeViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 class HomeViewModel: ObservableObject {
-    @Published var left: [PhotoItem] = [PhotoItem(height: 474, name: "cat"), PhotoItem(height: 375, name: "hair")]
+    @Published var left: [PhotoItem] = [PhotoItem(height: 474, name: "cat"),  PhotoItem(height: 375, name: "COW!!!!"), PhotoItem(height: 375, name: "room"), PhotoItem(height: 375, name: "hair")]
     @Published var right: [PhotoItem] = [PhotoItem(height: 253, name: "necklace"), PhotoItem(height: 377, name: "hk"),PhotoItem(height: 474, name: "zine")]
     
 }
