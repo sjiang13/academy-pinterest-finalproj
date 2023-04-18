3d30cd7252f88451d0803bc583d437aa91aab613
Sophie Jiang <sjiang13@email.unc.edu>
2023-04-18 00:39:21 -0400

    initial commit

---
+++ academy-pinterest-finalproj/academy-pinterest-finalproj/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  academy-pinterest-finalproj
+//
+//  Created by Sophie Jiang on 4/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ academy-pinterest-finalproj/academy-pinterest-finalproj/academy_pinterest_finalprojApp.swift
@@ -0,0 +1,17 @@
+//
+//  academy_pinterest_finalprojApp.swift
+//  academy-pinterest-finalproj
+//
+//  Created by Sophie Jiang on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct academy_pinterest_finalprojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
